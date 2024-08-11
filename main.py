1159ecdf6ab35acef54d032ca9c25023d30b31f3
Tom Poidevin <tom.poidevin@gmail.com>
2024-08-11 14:48:54 +0200

    abstract directory for Windows support and other OS

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from os import walk
 from os.path import splitext
 from pydub import AudioSegment, effects
+import pathlib
 
-input_path = './input/'
-output_path = './output/'
+cwd = pathlib.Path.cwd()
+input_folder = cwd / 'input'
+output_folder = cwd / 'output'
+
+if not output_folder.exists():
+  output_folder.mkdir(exist_ok=True)
+
 supported_formats = [".mp3", ".wav", ".aac", ".flac", ".ogg"]
 
 def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
@@ -12,21 +18,23 @@ def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
  
 def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
  
-def get_file_list(path = "."):  
+def get_file_list(path):  
   f = []
   for (dirpath, dirnames, filenames) in walk(path):
       f.extend(filenames)
       break
   return f
 
-inputs = get_file_list(input_path)
+inputs = get_file_list(input_folder)
   
 for (filename) in inputs:
   extension = splitext(filename)[1]
   if extension in(supported_formats):
-    rawsound = AudioSegment.from_file(input_path + filename, extension[1:])
+    inputFile = input_folder/filename
+    outputFile = output_folder/filename
+    rawsound = AudioSegment.from_file(inputFile, extension[1:])
     normalized = effects.normalize(rawsound)
-    normalized.export(output_path + filename, format=extension[1:]) 
+    normalized.export(outputFile, format=extension[1:]) 
     prGreen("Success: Normalized " + filename)
   else:
     prYellow("Warning:")
